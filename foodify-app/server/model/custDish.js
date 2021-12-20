765985838f04da93c70394332c45cb80c17b9674
Ivan Balahun <ivan.balahun@gmail.com>
2021-12-20 05:24:36 +0200

    finished main implementations

--- foodify-app/server/model/custDish.js
+++ foodify-app/server/model/custDish.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const CustomDishSchema = new mongoose.Schema({
   dishName: { type: String, required: true },
   recipe: { type: String, required: true },
-  img: { type: String},
 });
 
 module.exports = mongoose.model("CustomDish", CustomDishSchema);
